0a861942c1eb035e0615deed94b855ddd91e7b6e
Patrick Dronk <p.dronk@student.fontys.nl>
2017-01-10 14:59:01 +0100

    Update AttachmentController.js

--- app/Http/Controllers/AttachmentController.js
+++ app/Http/Controllers/AttachmentController.js
@@ -24,7 +24,7 @@ class AttachmentController {
 
     const attachment = request.file('files', {
       maxSize: '10mb',
-      allowedExtensions: ['jpg', 'png', 'jpeg']
+      allowedExtensions: ['jpg', 'png', 'jpeg', 'JPG']
     });
 
     const fileName = uuid.v4();
